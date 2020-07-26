c9c1ccc6039247deb5e0f7a53bcdc1dbd2120dc1
Kunal Banerjee <kunalbanerjee@gmail.com>
2020-07-26 22:35:07 +0000

    hard coded conversion

--- js/test.js
+++ js/test.js
@@ -1,14 +1,30 @@
-function ConvertNow(){
 
-        String[] authors = request.getParameterValues("typeFrom");
-        if (authors != null) {
-          for (int i = 0; i < authors.length; ++i) {
-            alert("You have selected author(s):")
-          }
-        }
-        return false
-      }
       function AlertNow(){
-            alert("You have selected author(s):")
+        var convertAmount = document.getElementById("amount").value;
+        var cAmount = /^[1-9]\d*(\.\d+)?$/;
+        if (convertAmount.match(cAmount)) {
+          const rbFromType = document.querySelectorAll('input[name="fromType"]');
+          let selFromType;
+          for(const rb of rbFromType){
+            if(rb.checked){
+              selFromType = rb.value;
+              break;
+            }
+          }
+
+          const rbToType = document.querySelectorAll('input[name="toType"]');
+          let selToType;
+          for(const rb of rbToType){
+            if(rb.checked){
+              selToType = rb.value;
+              break;
+            }
+          }
+          alert("akk wekk")
+          alert(convertAmount + " " + selFromType + " is " + selToType + " " +  "1000")
+        }
+        else{
+          alert("Invalid Amount")
+        }
         return false
       }
